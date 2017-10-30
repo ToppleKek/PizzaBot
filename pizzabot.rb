74d2e3c59927fbcca01bde6d813e30ff901fd729
TopKek <topkek@kekkles-debian.moviefree.faith>
2017-10-29 20:45:03 -0400

    added stats and updated example config

--- pizzabot.rb
+++ pizzabot.rb
@@ -14,19 +14,29 @@ bot.ready do |event|
     pizza = false
     event.bot.game = CONFIG.game
 
-    pizzatime = timers.every(Random.rand(5000..8000)) {
+    pizzatime = timers.every(Random.rand(6500..9000)) {
         channeltest = bot.find_channel('pizza', type: 0)
+        success_channels = []
         channeltest.each do |chan|
             puts chan.id
             time = Time.now
+
             thour = '%02d' % time.hour
             tmin = '%02d' % time.min
             begin
                 bot.send_file(chan.id, File.new( 'papa1.jpg' ), caption: "It's #{thour}:#{tmin} so its pizza time!!!!!")
             rescue Discordrb::Errors::NoPermission
                 puts "Mamma mia! I don't have permission to send to the pizza channel of server: #{chan.server.name} with serverid of: #{chan.server.id} and channelid of: #{chan.id}"
+            else
+                if CONFIG.stats
+                    success_channels.push(chan.server.name)
+                    #bot.send_message(CONFIG.stats_chan, "PizzaBot just delivered #{bot.servers.count} pizza's to:")
+                end
             end
         end
+        
+        bot.send_message(CONFIG.stats_chan, "PizzaBot just delivered #{bot.servers.count} pizza's to:\n #{success_channels.join(', ')}\n"\
+                                            "#{bot.users.count} people ate the pizza!")
     }
     
     loop { timers.wait }
@@ -40,7 +50,8 @@ bot.command(:ping) do |event|
 end
 
 bot.command(:invite) do |event|
-    bot.send_file(event.channel.id, File.new( 'papa2.jpg' ), caption: "Mamma mia! It's not pizza time in your server yet?!? "\
+    bot.send_file(event.channel.id, File.new( 'papa2.jpg' ), caption: 
+    "Mamma mia! It's not pizza time in your server yet?!? "\
     "Invite the bot with:\n<#{bot.invite_url}>\nand make a text channel called pizza!\n"\
     'This bot on github: <https://github.com/ToppleKek/PizzaBot>')
 end
